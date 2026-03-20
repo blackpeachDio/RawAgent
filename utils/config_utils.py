0c1bc1cb1a661f51284171df6c9a2d7786959eed
spade <243161582@qq.com>
2026-03-21 01:31:34 +0800

    feat: rag服务实现

--- utils/config_utils.py
+++ utils/config_utils.py
@@ -7,22 +7,22 @@ import yaml
 from utils.path_utils import get_abs_path
 
 
-def load_rag_config(config_path: str = get_abs_path("config/rag.yml"), encoding: str = "utf-8"):
+def load_rag_config(config_path: str = get_abs_path("../config/rag.yml"), encoding: str = "utf-8"):
     with open(config_path, "r", encoding=encoding) as f:
         return yaml.load(f, Loader=yaml.FullLoader)
 
 
-def load_chroma_config(config_path: str = get_abs_path("config/chroma.yml"), encoding: str = "utf-8"):
+def load_chroma_config(config_path: str = get_abs_path("../config/chroma.yml"), encoding: str = "utf-8"):
     with open(config_path, "r", encoding=encoding) as f:
         return yaml.load(f, Loader=yaml.FullLoader)
 
 
-def load_prompts_config(config_path: str = get_abs_path("config/prompts.yml"), encoding: str = "utf-8"):
+def load_prompts_config(config_path: str = get_abs_path("../config/prompts.yml"), encoding: str = "utf-8"):
     with open(config_path, "r", encoding=encoding) as f:
         return yaml.load(f, Loader=yaml.FullLoader)
 
 
-def load_agent_config(config_path: str = get_abs_path("config/agent.yml"), encoding: str = "utf-8"):
+def load_agent_config(config_path: str = get_abs_path("../config/agent.yml"), encoding: str = "utf-8"):
     with open(config_path, "r", encoding=encoding) as f:
         return yaml.load(f, Loader=yaml.FullLoader)
 
